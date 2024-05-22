935570fe9a2af0b6390b9972361457ae81addd73
Daniel Eystberg Sørensen Anderson <danieleystberg@daniels-mbp.lan>
2024-05-22 11:17:56 +0200

    Added clerk

---
+++ src/app/_components/page.tsx
@@ -0,0 +1,13 @@
+import { SignInButton, SignOutButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+
+export function TopNav() {
+    return (
+      <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
+        <div>Some ??</div>
+        <div>
+          <SignedOut><SignInButton /></SignedOut>
+          <SignedIn><UserButton /></SignedIn>
+        </div>
+      </nav>
+    )
+  }
