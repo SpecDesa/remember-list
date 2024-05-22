5d9e7ee9492564c3ad977903357a307aa7f96dfa
Daniel Eystberg Sørensen Anderson <danieleystberg@daniels-mbp.lan>
2024-05-22 09:50:38 +0200

    Scaffold ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,33 @@
 import Link from "next/link";
-
+import Image from 'next/image'
 export default function HomePage() {
+  
+ const mockUrls = [
+  "https://utfs.io/f/fc293c6e-7ba9-4281-a1f7-99965314538c-v2jhnj.png",
+  "https://utfs.io/f/fc293c6e-7ba9-4281-a1f7-99965314538c-v2jhnj.png",
+  "https://utfs.io/f/fc293c6e-7ba9-4281-a1f7-99965314538c-v2jhnj.png",
+  "https://utfs.io/f/fc293c6e-7ba9-4281-a1f7-99965314538c-v2jhnj.png",
+] 
+
+
+ const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+ }))
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello IN PROGRESS
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {
+        [...mockImages, ...mockImages, ...mockImages, ...mockImages].map((image) => (
+          <div key={image.url} className="w-48">
+            <Image 
+              width={200}
+              height={200} 
+              src={image.url} alt="image" />
+            </div>
+        ))
+        }
+      </div>
     </main>
   );
 }
