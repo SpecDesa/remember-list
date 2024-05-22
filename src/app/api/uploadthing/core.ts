e22054d0cb971dec642a46296366bd3378521ca0
Daniel Eystberg Sørensen Anderson <danieleystberg@daniels-mbp.lan>
2024-05-22 20:14:33 +0200

    Added clerk metadata to user for upload permission

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,4 +1,4 @@
-import { auth } from "@clerk/nextjs/server";
+import { auth, clerkClient } from "@clerk/nextjs/server";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { UploadThingError } from "uploadthing/server";
 import { db } from "~/server/db";
@@ -21,6 +21,12 @@ export const ourFileRouter = {
       // If you throw, the user will not be able to upload
       if (!user.userId) throw new UploadThingError("Unauthorized");
       
+
+      // Get metadata 
+      const fullUserData = await clerkClient.users.getUser(user.userId);
+
+      if(fullUserData?.privateMetadata?.["can-upload"] !== true)
+        throw new UploadThingError("User does not have upload permissions");
 
       // import and use ratelimiter
       const {success} = await ratelimit.limit(user.userId);
