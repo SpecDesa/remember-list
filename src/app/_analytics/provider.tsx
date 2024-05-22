6f5cfd6ee69ae7eda8bcb05f26e47cd64083f1ff
Daniel Eystberg Sørensen Anderson <danieleystberg@daniels-mbp.lan>
2024-05-22 20:23:20 +0200

    Played around with using env.js

--- src/app/_analytics/provider.tsx
+++ src/app/_analytics/provider.tsx
@@ -4,9 +4,10 @@ import { useAuth, useUser } from "@clerk/nextjs";
 import posthog from "posthog-js";
 import { PostHogProvider } from "posthog-js/react";
 import { useEffect } from "react";
+import { env } from "~/env";
 
 if (typeof window !== "undefined") {
-  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+  posthog.init(env.NEXT_PUBLIC_POSTHOG_KEY, {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
   });
 }
