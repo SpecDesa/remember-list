7bc720eb4e71ff1138297f8ed7377e819e3c1a5f
Daniel Eystberg Sørensen Anderson <danieleystberg@daniels-mbp.lan>
2024-05-22 14:49:29 +0200

    Added sentry for error handling

---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://72cfa038a47ca58c74c9c0d7d189f571@o4507299840851969.ingest.de.sentry.io/4507299849633872",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+  
+});
