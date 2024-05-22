3f7d9b64cbaa0d0d7fc3fbab87083504fee465e5
Daniel Eystberg Sørensen Anderson <danieleystberg@daniels-mbp.lan>
2024-05-22 19:56:27 +0200

    Posthog added

---
+++ src/server/analytics.ts
@@ -0,0 +1,24 @@
+// app/posthog.js
+import { PostHog } from 'posthog-node'
+
+function serverSideAnalytics() {
+  const posthogClient = new PostHog(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+    host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    flushAt: 1,
+    flushInterval: 0
+  })
+  return posthogClient;
+}
+
+
+const analyticsServerClient = serverSideAnalytics();
+
+
+// Use by importing and do analyticsServerClient.capture({
+// distintId: user.userId,
+// event: "delete image"
+// properties: {
+//  taskId: id,
+// }
+// })
+export default analyticsServerClient;
