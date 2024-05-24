2d2cbfbf38166b7d17aacf1c205559cabbfce1ab
Daniel Eystberg Sørensen Anderson <danieleystberg@Daniels-MacBook-Pro.local>
2024-05-24 14:43:10 +0200

    Testing print of headers

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -1,12 +1,14 @@
 
 
 import { NextResponse } from "next/server";
-
+import { headers } from "next/headers";
 export const dynamic = "force-dynamic";
 
 // A faulty API route to test Sentry's error monitoring
 export function POST(request: Request) {
-    console.log("TEST")
+    const requestHeaders = headers();
+    const authValue = requestHeaders.get("AuthorizationClerk")
+    console.log("TEST", authValue)
     console.log("Header;", request.headers)
 
     console.log(request.body)
